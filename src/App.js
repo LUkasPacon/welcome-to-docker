f068e26a7a1850a01d2c267a9c27a2b7ed9f817b
LUkasPacon <104427214+LUkasPacon@users.noreply.github.com>
2024-01-29 10:12:53 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ const App = () => {
       <header className="App-header">
         <h1 style={{ marginBottom: "0px" }}>Congratulations!!!</h1>
         <p style={{ marginTop: "10px", marginBottom: "50px" }}>
-          You ran your first containerrrrrrrr.
+          You ran your first containerrrrrrrr-Lukáškův první Docker.
         </p>
         <div>
           <a
